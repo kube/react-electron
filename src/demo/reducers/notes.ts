80c3fb4fd0e0a5c8b69fe5e8f9db5b456f0148e4
unknown <christophe@feijoo.fr>
2017-03-31 01:01:44 +0200

    Remove use of when-switch

--- src/demo/reducers/notes.ts
+++ src/demo/reducers/notes.ts
@@ -1,4 +1,3 @@
-import when from 'when-switch'
 import { set, setAppend } from 'monolite'
 import { NoteAction } from '../actions/notes'
 
@@ -17,19 +16,24 @@ const INITIAL_NOTES: Note[] = [
 ]
 
 const notesReducer =
-  (notes: Note[] = INITIAL_NOTES, action: NoteAction): Note[] =>
-    when(action.type)
-      .is('UPDATE_TITLE', () =>
-        set(notes, _ => _[action.payload.id].title)(action.payload.value)
-      )
-      .is('CLOSE_NOTE', () =>
-        set(notes, _ => _)(notes =>
+  (notes: Note[] = INITIAL_NOTES, action: NoteAction): Note[] => {
+
+    switch (action.type) {
+      case 'UPDATE_TITLE':
+        return set(notes, _ => _[action.payload.id].title)(action.payload.value)
+
+      case 'CLOSE_NOTE':
+        return set(notes, _ => _)(notes =>
           notes.filter(note => note.id !== action.payload)
         )
-      )
-      .is('ADD_NOTE', () =>
-        setAppend(notes, _ => _)(action.payload)
-      )
-      .else(notes)
+
+      case 'ADD_NOTE':
+        return setAppend(notes, _ => _)(action.payload)
+
+      default:
+        return notes
+    }
+  }
+
 
 export default notesReducer
